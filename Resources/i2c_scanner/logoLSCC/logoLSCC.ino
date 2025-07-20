f456551ee0736cae43d8f60ca20e24e002e85300
Rakib Hasan <kmrakib168@gmail.com>
2025-07-21 00:17:15 +0600

    Add project Height_measurement

--- Resources/i2c_scanner/logoLSCC/logoLSCC.ino
+++ Resources/i2c_scanner/logoLSCC/logoLSCC.ino
@@ -16,6 +16,7 @@ const unsigned char batman [] PROGMEM = {
 // ------------------- For i2c -------------------
 //// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
 #define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
  
 // ------------------- For SPI -------------------
@@ -43,7 +44,7 @@ void setup() {
  
 //  ------------------- For i2c -------------------
 //  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
     Serial.println(F("SSD1306 allocation failed"));
     for(;;); // Don't proceed, loop forever
   }
